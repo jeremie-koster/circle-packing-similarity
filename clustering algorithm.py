daf9a0e72cd509ed5fb4fb7f2f1d2347cc396f67
Jeremie <Jrmie Koster>
2018-06-14 15:54:29 +0100

    a few corrections, 4 circles OK, 5 not OK

--- clustering algorithm.py
+++ clustering algorithm.py
@@ -36,23 +36,31 @@ class LeafCircle:
         self.id = id
         self.x = x
         self.y = y
-        self.homogeneous_coord = np.array([[self.x],[self.y],[1]])
         self.r = radius
         
+    def getHomoCoord(self):
+        return np.array([[self.x],[self.y],[1]])
+    
     def joinWith (self,circle_to_join): # general function
         if (circle_to_join.id < number_of_leaves): # if joining singleton with singleton
             self.joinToSingleton(circle_to_join)
-            print("Join done!")
+            print("Join done! 1 + 1 done!")
         elif (circle_to_join.id >= number_of_leaves): # or if joining singleton with cluster
             circle_to_join.addSingleton(self)
-            print("Join done!")
+            list_of_clusters.remove(self)
+            print("Join done! n + 1 done!")
         
     def joinToSingleton (self,circle_to_add): # 1 + 1
         global counter_for_cluster_id
         circle_to_add.x = self.r + circle_to_add.r # 2nd circle touches 1st on 1 point 
         circle_to_add.y = 0 # 2nd circle on x axis
-        list_of_clusters.append(Cluster(counter_for_cluster_id,self,circle_to_add))
-        counter_for_cluster_id =+ 1
+        list_of_clusters.append(Cluster(counter_for_cluster_id,self,circle_to_add)) # create new cluster
+        print("homo coord",circle_to_add.getHomoCoord())
+        print("x of circle to add",circle_to_add.x)
+        list_of_clusters.remove(self) # and remove both leaf objects from list of clusters
+        list_of_clusters.remove(circle_to_add) # same
+        counter_for_cluster_id += 1
+        print("counter cluster id:",counter_for_cluster_id)
            
 class Cluster:
     def __init__(self,id,LeafCircle1,LeafCircle2):
@@ -65,7 +73,8 @@ class Cluster:
         """ Function to use when 'creating' new cluster after a join of 2 sub-clusters. Instead of deleting old cluster and create a new one after the join, we change the id and increment the counter for later use """
         global counter_for_cluster_id
         self.id = new_id
-        counter_for_cluster_id =+ 1
+        counter_for_cluster_id += 1
+        print("counter cluster id:",counter_for_cluster_id)
         
     def add_circle_to_cluster (self,circle_to_add):
         self.circles.append(circle_to_add)
@@ -73,42 +82,67 @@ class Cluster:
     def joinWith(self,circle_to_join):
         if (circle_to_join.id < number_of_leaves): # joining cluster with singleton
             self.addSingleton(circle_to_join)
-            print("Join done!")
+            print("length of list cluster before removal of singleton ",len(list_of_clusters))
+            list_of_clusters.remove(circle_to_join)
+            print("length of list cluster after removal of singleton, should be -1 -> ",len(list_of_clusters))
+            print("Join done! Singleton added to cluster!")
         elif (circle_to_join.id >= number_of_leaves): # joining cluster with cluster
             print("Not developped yet") # TODO: function n + n (see Mike's idea)
-            print("Join done!")
+#            print("Join done!")
         
     def addSingleton(self,singleton): # n + 1 function
         """ Function that adds a singleton to a cluster. 
         For each pair of the cluster, if the pair is not too distant, do 1+2 function and store the 2 possibilities. Then, exclude all overlapping elements and choose best possib. Adds the circle object to the list of circles of the cluster, and 'creates' new cluster by changing id   """
         list_of_possib = [] # list of possibilities (should contain LeafCircle objects)
+        i = 0
+        print("There are {0} circles in the cluster".format(len(self.circles)))
         for pair in it.combinations(self.circles,2):
             if ((pair[1].x - pair[0].x)**2 + (pair[1].y - pair[0].y)**2 <= ((2*singleton.r) + pair[0].r + pair[1].r)**2): # check if the circles of the pair are not too distant from each other
                 list_of_possib.extend(twoPlusOne(pair,singleton)) # add the 2 possib when doing 2 + 1
+        print("There are {0} elem in list of possib".format(len(list_of_possib)))
         for element in list_of_possib: # remove overlapping elements
+            print("{0}th element is going to be checked for intersection".format(i))
+            i += 1
             if (checkIntersection(self,element) == True):
+                print("length list of possib before removal",len(list_of_possib))
                 list_of_possib.remove(element) # TO TEST
+                print("length list of possib after removal",len(list_of_possib))
+        print("{0} elem have been checked".format(i))
+# 2nd round of checking intersection
+        i = 0
+        print("2ND ROUND -> There are now {0} elem in list of possib".format(len(list_of_possib)))
+        for element in list_of_possib: # remove overlapping elements
+            print("{0}th element is going to be checked for intersection".format(i))
+            i += 1
+            if (checkIntersection(self,element) == True):
+                print("length list of possib before removal",len(list_of_possib))
+                list_of_possib.remove(element) # TO TEST
+                print("length list of possib after removal",len(list_of_possib))
+        print("{0} elem have been checked ----- END OF CHECK".format(i))        
+        
         self.add_circle_to_cluster(self.chooseBestPossib(list_of_possib)) # take best possib and add it to the cluster
         self.changeId(counter_for_cluster_id) # pretend to create new cluster. Instead, change cluster id and increment counter
     
     def chooseBestPossib(self,list):
         """ Function that computes the best possibility to place a new circle in a cluster to have a circle shape. Requires a Cluster object and a list of LeafCircle possibilities """
         longest_distances = [] # list of the biggest distances for each possibility
+        
         for possib in list:
-            list_of_distances = []
-            for circle in self:
+            list_of_distances = [] # contains the longest dist for the current possib
+            for circle in self.circles:
                 d = sqrt((possib.y - circle.y)**2 + (possib.x - circle.x)**2)    
                 if (len(list_of_distances) == 0): # for the first iteration
-                    list_of_distances.append((d,circle))
+                    list_of_distances.append((d,possib))
                 elif (d > list_of_distances[0][0]): # replace element if d is bigger   
                     list_of_distances.pop(0)
-                    list_of_distances.append((d,circle))
+                    list_of_distances.append((d,possib))
             longest_distances.append(list_of_distances[0])
+        
         while len(longest_distances) > 1: # extract the LeafCircle corresp. to small dist
-            if (longest_distances[0][0] > longest_distances[1][0]):
+            if (longest_distances[0][0] >= longest_distances[1][0]):
                 del longest_distances[0]
             elif (longest_distances[0][0] < longest_distances[1][0]):
-                del longest_distances[1][0]
+                del longest_distances[1]
         return longest_distances[0][1] # return LeafCircle object corresponding to smallest dist
     
 # ------------------------------------------------------------------- #     
@@ -119,7 +153,7 @@ def runLinkageTable():
     while len(clean_linkage_matrix) > 0: # as long as LT is not empty...
         id_of_first_cluster = clean_linkage_matrix[0][0]
         id_of_second_cluster = clean_linkage_matrix[0][1]
-        print("I want to join {0} with {1}".format(id_of_first_cluster,id_of_second_cluster))
+        print("-------I want to join {0} with {1}------".format(id_of_first_cluster,id_of_second_cluster))
         for c in list_of_clusters:
             for d in list_of_clusters:
                 if ((c.id == id_of_first_cluster) & (d.id == id_of_second_cluster)):
@@ -144,14 +178,18 @@ def checkIntersection(cluster,circle_to_check):
     circles_in_cluster = cluster.circles # list of circles (as LeafCircle objects) that are in the cluster to which we want to check any intersection
     intersec = False
     for i in circles_in_cluster:
-        if ((x_to_check - i.x)**2 + (y_to_check - i.y)**2 < (r_to_check + i.r)**2):
+        if (sqrt((x_to_check - i.x)**2 + (y_to_check - i.y)**2) < (r_to_check + i.r)):
             intersec = True
+            print("circle is intersecting")
+#    print("partie gauche",(x_to_check - i.x)**2 + (y_to_check - i.y)**2)
+#    print("partie droite",((r_to_check + i.r)**2))
+    print("bool check intersection ->",intersec)
     return intersec
     
 def twoPlusOne(cluster,singleton): # 2 + 1 function
     """ Function that computes coordinates (x,y) and (x',y') of 3rd circle C3 that touches the 2 other circles C1 and C2. Takes as parameters tuples of the coordinates and radius of C1 and C2, and the radius of C3 """
-    C1 = cluster[0].homogeneous_coord
-    C2 = cluster[1].homogeneous_coord
+    C1 = cluster[0].getHomoCoord()
+    C2 = cluster[1].getHomoCoord()
     r1,r2,r3 = cluster[0].r, cluster[1].r, singleton.r
     list_to_return = [] # will contain 2 LeafCircle objects of the 2 solutions
 
@@ -163,13 +201,12 @@ def twoPlusOne(cluster,singleton): # 2 + 1 function
     C3_symmetric_before_transformation = np.array([[x],[y_symmetric],[1]])
     
     # rotating and translating (transformation) the result for general case
-    alpha = atan2(C2.y-C1.y,C2.x-C1.x)
-    matrix = transform([C1.x,C1.y],alpha)
+    alpha = atan2(C2[1]-C1[1],C2[0]-C1[0])
+    matrix = transform([C1[0],C1[1]],alpha)
     C3 = matrix@C3_before_transformation
     C3_symmetric = matrix@C3_symmetric_before_transformation
-    print("C3 : ",C3)
-    print("C3 symmetric : ",C3_symmetric)
-
+#    print("C3 : {0},{1},{2}".format(C3[0][0],C3[1][0],r3))
+#    print("C3 symmetric : {0},{1},{2}".format(C3_symmetric[0][0],C3_symmetric[1][0],r3))
 # =============================================================================
 #     circle = plt.Circle((C3[0],C3[1]),r3,ec = 'r',fill=False)
 #     ax2.add_artist(circle)
@@ -177,9 +214,8 @@ def twoPlusOne(cluster,singleton): # 2 + 1 function
 #     ax2.add_artist(circle2)
 #     plt.savefig("four circles")
 # =============================================================================
-    
-    list_to_return.append(LeafCircle('no_id',C3[0][0],C3[1][0],r3))
-    list_to_return.append(LeafCircle('no_id',C3_symmetric[0][0],C3[1][0],r3))
+    list_to_return.append(LeafCircle(singleton.id,C3[0][0],C3[1][0],r3))
+    list_to_return.append(LeafCircle(singleton.id,C3_symmetric[0][0],C3_symmetric[1][0],r3))
     
     return list_to_return        
     
@@ -246,13 +282,21 @@ set_link_color_palette(['m', 'c', 'y', 'k'])
 
 dendrogram = dendrogram(linkage_matrix)
 
+runLinkageTable()
+
 # set up figure n°2 for the circle packing layout
 plt.figure(2, figsize=(10,10))
 ax2 = plt.subplot(223)
-plt.axis([-0.1,1,0,1])
+plt.axis([-0.1,0.5,-0.3,0.5])
 ax2.set_aspect('equal')
 
-runLinkageTable()
+for circle in list_of_clusters[0].circles:
+    x = circle.x
+    y = circle.y
+    r = circle.r
+    circle = plt.Circle((x,y),r,fill=False)
+    ax2.add_artist(circle)
+
 
 # ----- END ------- #
 
